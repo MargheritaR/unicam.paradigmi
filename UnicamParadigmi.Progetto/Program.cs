4210eb4fa0353230fe7f4334ebe23081d6fdc9b2
MargheritaR <margherit.ramazzotti@studenti.unicam.it>
2024-04-02 17:59:20 +0200

    Creazione Progetto

---
+++ UnicamParadigmi.Progetto/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace UnicamParadigmi.Progetto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
